af63f68112abfdddb32464a044f1caa0f08e4ae8
Austen Lamacraft <austen@lamacraft.io>
2018-01-11 12:48:29 +0000

    My solution

--- src/poker.py
+++ src/poker.py
@@ -1,5 +1,6 @@
 from itertools import product
 from random import shuffle
+from collections import Counter
 
 VALUES = ('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
 SUITS = ('H', 'D', 'S', 'C')
@@ -56,6 +57,34 @@ class PokerHand:
             raise ValueError
         self.cards = sorted(cards)
 
+
+    @property
+    def hand_type(self):
+        value_counts = Counter([card.value for card in self.cards]).most_common()
+        value_spread = self.cards[4].value_index - self.cards[0].value_index
+        suit_counts = Counter([card.suit for card in self.cards])
+
+
+        if value_spread == 4 and len(suit_counts) == 1:
+            return "Straight Flush", self.cards[-1].value
+        elif value_counts[0][1] == 4:
+            return "Four of a Kind", self.cards[-1].value
+        elif value_counts[0][1] == 3 and value_counts[1][1] == 2:
+            return "Full House", self.cards[-1].value
+        elif len(suit_counts) == 1:
+            return "Flush", self.cards[-1].value
+        elif value_spread == 4 and len({card.value for card in self.cards}) == 5:
+            return "Straight", self.cards[-1].value
+        elif value_counts[0][1] == 3:
+            return "Three of a Kind", value_counts[0][0]   # Value of the three
+        elif value_counts[0][1] == 2 and value_counts[1][1] == 2:
+            return "Two Pair", max(value_counts[0][0], value_counts[1][0]) #Value of higher pair, which may be second...
+
+        elif value_counts[0][1] == 2:
+            return "Pair", value_counts[0][0]
+        else:
+            return "High Card", self.cards[-1].value
+
     def __repr__(self):
         return '<PokerHand object {}>'.format(', '.join(str(card) for card in self))
 
@@ -63,10 +92,16 @@ class PokerHand:
         return iter(self.cards)
 
     def __gt__(self, other):
-        return self.cards[-1] > other.cards[-1]
+        if self.hand_type[0] != other.hand_type[0]:
+            return HAND_TYPES.index(self.hand_type[0]) > HAND_TYPES.index(other.hand_type[0])
+        else:
+            return VALUES.index(self.hand_type[1]) > VALUES.index(other.hand_type[1])
 
     def __lt__(self, other):
-        return self.cards[-1] < other.cards[-1]
+        if self.hand_type[0] != other.hand_type[0]:
+            return HAND_TYPES.index(self.hand_type[0]) < HAND_TYPES.index(other.hand_type[0])
+        else:
+            return VALUES.index(self.hand_type[1]) < VALUES.index(other.hand_type[1])
 
     def __eq__(self, other):
         return self.cards[-1] == other.cards[-1]
--- src/tests.py
+++ src/tests.py
@@ -21,6 +21,43 @@ assert card_1 != card_3
 assert card_1 > card_3
 assert not card_1 < card_3
 
+# test identifying hands
+cards_1 = [Card(v, s) for v, s in [(A, S), (2, D), (4, C), (7,  H), (K, S)]]
+high_card = PokerHand(cards_1)
+assert high_card.hand_type == ("High Card", "A")
+
+cards_2 = [Card(v, s) for v, s in [(A, S), (Q, S), (J, S), (T,  S), (K, S)]]
+straight_flush = PokerHand(cards_2)
+assert straight_flush.hand_type == ("Straight Flush", "A")
+
+cards_3 = [Card(v, s) for v, s in [(A, S), (A, D), (A, H), (A,  C), (K, S)]]
+four = PokerHand(cards_3)
+assert four.hand_type == ("Four of a Kind", "A")
+
+cards_4 = [Card(v, s) for v, s in [(A, S), (A, D), (A, H), (K,  C), (K, S)]]
+full_house = PokerHand(cards_4)
+assert full_house.hand_type == ("Full House", "A")
+
+cards_5 = [Card(v, s) for v, s in [(A, S), (J, S), (8, S), (4,  S), (2, S)]]
+flush = PokerHand(cards_5)
+assert flush.hand_type == ("Flush", "A")
+
+cards_6 = [Card(v, s) for v, s in [(6, D), (5, S), (4, H), (3,  S), (2, S)]]
+straight = PokerHand(cards_6)
+assert straight.hand_type == ("Straight", "6")
+
+cards_7 = [Card(v, s) for v, s in [(6, D), (6, S), (6, H), (3,  D), (2, S)]]
+three = PokerHand(cards_7)
+assert three.hand_type == ("Three of a Kind", "6")
+
+cards_8 = [Card(v, s) for v, s in [(6, D), (6, S), (3, H), (3, D), (2, S)]]
+two_pair = PokerHand(cards_8)
+assert two_pair.hand_type == ("Two Pair", "6")
+
+cards_9 = [Card(v, s) for v, s in [(6, D), (6, S), (5, H), (3, D), (2, S)]]
+pair = PokerHand(cards_9)
+assert pair.hand_type == ("Pair", "6")
+
 # test comparing two poker hands
 cards = [Card(v, s) for v, s in [(A, S), (2, D), (4, C), (7,  H), (K, S)]]
 hand_1 = PokerHand(cards)
@@ -35,6 +72,29 @@ cards = [Card(v, s) for v, s in [(Q, S), (3, D), (5, C), (8,  H), (T, S)]]
 hand_2 = PokerHand(cards)
 assert hand_1 > hand_2
 assert hand_2 < hand_1
+
+# Compare two hands better than High Card
+assert full_house > flush
+assert flush < full_house
+
+assert straight_flush > four
+assert four < straight_flush
+
+# Compare same hands
+pair_1_cards = [Card(v, s) for v, s in [(6, D), (6, S), (5, H), (3, D), (2, S)]]
+pair_1 = PokerHand(pair_1_cards)
+pair_2_cards = [Card(v, s) for v, s in [(7, D), (7, S), (5, H), (3, D), (2, S)]]
+pair_2 = PokerHand(pair_2_cards)
+assert pair_2 > pair_1
+assert pair_1 < pair_2
+
+straight_flush_1_cards = [Card(v, s) for v, s in [(A, S), (Q, S), (J, S), (T,  S), (K, S)]]
+straight_flush_2_cards = [Card(v, s) for v, s in [(K, S), (J, S), (T, S), (9,  S), (Q, S)]]
+straight_flush_1 = PokerHand(straight_flush_1_cards)
+straight_flush_2 = PokerHand(straight_flush_2_cards)
+assert straight_flush_1 > straight_flush_2
+assert straight_flush_2 < straight_flush_1
+
 
 # test creating a deck
 deck = create_deck()
